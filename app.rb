28644172b582c425914510a2708285a7f01a8ceb
Noah Huppert <noahhuppert@gmail.com>
2015-05-15 16:40:39 -0400

    double > single quotes,+yardoc

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
-require "rubygems"
-require "bundler" 
+require 'rubygems'
+require 'bundler'
 
 Bundler.require
 
-require "./app/base"
+require './app/base'
 
+# The global module for Onyx, all Onyx related code will be in the Onyx module
+# @author Noah Huppert <developer.noah@gmail.com>
 module Onyx
+	# The main entrypoint for Onyx
+	# @author Noah Huppert <developer.noah@gmail.com>
 	class App < Base
-		get "/" do
-			return "Root"
+		get '/' do
+			return 'Root'
 		end
-	end#class app
-end#module Onyx
+	end# class app
+end# module Onyx
--- app/base.rb
+++ app/base.rb
@@ -1,7 +1,9 @@
 module Onyx
+	# A Sinatra application template with common settings
+	# @author Noah Huppert <developer.noah@gmail.com>
 	class Base < Sinatra::Base
 		enable :sessions, :static
-		set :static, Proc.new { File.join(root, "public") }
-		set :port, ENV["PORT"] || 5000
-	end#class Base
-end#module Onyx
+		set :static, Proc.new { File.join(root, 'public') }
+		set :port, ENV['PORT'] || 5000
+	end# class Base
+end# module Onyx

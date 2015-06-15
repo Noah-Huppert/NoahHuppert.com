f67878b41f68d4d1118ad295ec7aa3f06eadadbb
Noah Huppert <noahhuppert@gmail.com>
2015-06-14 22:29:41 -0400

    Fixed config to use env vars

--- app/config/base.rb
+++ app/config/base.rb
@@ -25,8 +25,18 @@ module Onyx
     end
 
     @CONFIG = YAML.load_file(File.join(__dir__, 'config.yml'))[ENV['RACK_ENV']]
+
+    if ENV['RACK_ENV'] == 'production'
+      @CONFIG['host'] = ENV['OPENSHIFT_mysql_DB_HOST']
+      @CONFIG['port'] = ENV['OPENSHIFT_mysql_DB_PORT']
+      @CONFIG['username'] = ENV['OPENSHIFT_mysql_DB_USERNAME']
+      @CONFIG['password'] = ENV['OPENSHIFT_mysql_DB_PASSWORD']
+    end
+
     @PERMISSIONS = YAML.load_file(File.join(__dir__, 'permissions.yml'))
     @PERMISSION_GROUPS = load_permission_groups
+
+    puts "#{YAML.load_file(File.join(__dir__, 'config.yml'))['production']}"
 
     class << self
       attr_accessor :CONFIG, :PERMISSIONS, :PERMISSION_GROUPS

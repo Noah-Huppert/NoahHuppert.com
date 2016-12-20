6e38de0e836256f3ac26662ce1f33a4f0fa80be8
Noah Huppert <noahhuppert@gmail.com>
2016-12-20 13:36:58 -0500

    removed old app files

--- app/models/base.rb
+++
@@ -1,9 +0,0 @@
-module Onyx
-  module Models
-    require './app/models/user'
-    require './app/models/provider_access_token'
-    require './app/models/api_access_token'
-    require './app/models/post'
-    require './app/models/post_meta'
-  end
-end

adad43fb1ec3df727f02ed7e41e476f4d2885609
Noah Huppert <noahhuppert@gmail.com>
2017-06-14 19:19:54 -0400

    Made auto chess instance smaller

--- deploy/auto-chess/002_instance.tf
+++ deploy/auto-chess/002_instance.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "auto_chess" {
     image = "coreos-alpha"
     name = "auto-chess"
     region = "${var.do_region}"
-    size = "2gb"
+    size = "512mb"
     ipv6 = true
     private_networking = true
     ssh_keys = ["${var.do_ssh_key_id}"]

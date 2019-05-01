0a758cbf700eace3a8f8c62d9c28ae0323ce5337
Noah Huppert <contact@noahh.io>
2019-04-30 23:09:10 -0400

    made site show list of projects

--- www/js/Projects.vue
+++ www/js/Projects.vue
@@ -1,9 +1,18 @@
-<template>
+><template>
 <section id="projects" class="padded content-max">
   <h2>Projects</h2>
   
-  <div v-for="project in orderedProjects">
-    {{ project.name }}
+  <div id="projects-container">
+    <div class="project" v-for="project in orderedProjects">
+      <h3>{{ project.name }}</h3>
+      <p>{{ project.content }}</p>
+      
+      <div class="languages">
+	<span v-for="lang in project.languages">
+	  {{ lang }}
+	</span>
+      </div>
+    </div>
   </div>
 </section>
 </template>
@@ -43,5 +52,54 @@ export default {
 #projects {
     padding-top: 0;
     margin-top: 5rem;
+}
+
+#projects-container {
+    display: flex;
+    flex-wrap: wrap;    
+}
+
+#projects h2 {
+    margin-bottom: 1rem;
+}
+
+.project {
+    width: 37rem;
+    min-height: 22rem;
+    flex: 0 1 37rem;
+    display: inline-block;
+    margin: 7rem;
+    padding: 1.5rem;
+    padding-bottom: calc(1.5rem + 3rem);
+    position: relative;
+    
+    border: 3px solid #000000;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.project h3 {
+    font-weight: bold;
+}
+
+.project p {
+    height: 100%;
+    font-size: 2rem;
+}
+
+.project .languages {
+    position: absolute;
+    bottom: 1.5rem;
+}
+
+.project .languages span {
+    padding: 0.8rem;
+    margin-right: 0.5rem;
+    display: inline-flex;
+    flex-direction: row;
+    box-sizing: border-box;
+    border: 0.2rem solid var(--color-blue);
+    border-radius: 0.3rem;
+    color: var(--color-blue);
 }
 </style>

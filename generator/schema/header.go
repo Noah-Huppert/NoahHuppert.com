93e6dc5dc87a6ef4bddd6848147808f8c0ca2df1
Noah Huppert <contact@noahh.io>
2019-07-13 17:42:58 -0400

    generator: put all inputfile schema in 1 file

--- generator/schema/header.go
+++
@@ -1,4 +0,0 @@
-package schema
-
-// Header holds metadata about an item
-type Header interface{}
--- generator/schema/inputfile.go
+++ generator/schema/inputfile.go
@@ -2,9 +2,28 @@ package schema
 
 // InputFile is 1 content file
 type InputFile struct {
-	// Header holds metadata
+	// Meta holds input file metadata.
+	// Field set before marshalling.
+	Meta
+	
+	// Header holds content metadata
+	// Field set during marshalling.
 	Header
 
 	// Content is markdown formatted
+	// Field set during marshalling.
 	Content string `json:"content"`
 }
+
+// Meta holds input file metadata
+type Meta struct {
+	// SchemaType is the name of the schema to apply to input file
+	SchemaType string
+
+	// TypeID is the name of the type OR type + subtype. The concept of subtypes
+	// is only used in this field. It is never used to assign a schema.
+	TypeID string
+}
+
+// Header holds metadata about an item
+type Header interface{}

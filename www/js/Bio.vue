1b95cd612d6f26eadbd3def9279f335da9fc1190
Noah Huppert <contact@noahh.io>
2021-01-02 04:52:59 -0500

    tweaked bio

--- www/js/Bio.vue
+++ www/js/Bio.vue
@@ -3,19 +3,23 @@
     <h2>About Me</h2>
 
     <p>
-      My name is Noah Huppert, I am a computer scientist who
-	 solves problems in the software and hardware development
-	 stack. My experience ranges from work in cloud computing
-	 and web development to firmware and electronics.
+      My name is Noah Huppert, I am a computer scientist
+	 who solves problems in the software and hardware
+	 development stack. My experience ranges from work
+	 in cloud computing and web development to firmware
+	 and electronics.
 	 <br><br>
-	 I enjoy spending time outdoors biking and rollerblading,
-	 snowboarding in the winter. Flight simulator and
-	 Cyberpunk 2077 are my games of choice at the moment. 
-	 Currently I run Void Linux on my laptop and server. I
-	 spend some of my free time working on side projects or
-	 contributing to other open source projects. I am about to
-	 complete my last semester at the University of
-	 Massachusetts Amherst.
+	 I enjoy spending time outdoors biking,
+	 rollerblading, and in the winter snowboarding.
+	 Flight simulator and Cyberpunk 2077 are my games of
+	 choice at the moment. Currently I run Void Linux
+	 on my laptop and server. I spend some of my free
+	 time working on side projects or contributing to
+	 other open source. I am about to complete
+	 my last semester at the University of Massachusetts
+	 Amherst. Where I am studying computer science,
+	 and also enjoy learning about political theory
+	 and foreign languages.
     </p>
 
     <p>
